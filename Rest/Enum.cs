242d37392b7f292c0861dfdf4228b8e90c852256
Alex Rondão <alexrondao@hotmail.com>
2019-10-03 11:31:37 -0300

    Classe de HttpRest Helper

---
+++ Rest/Enum.cs
@@ -0,0 +1,12 @@
+﻿namespace Printlaser.Infra.Crosscutting.Rest
+{
+    public enum RestApi
+    {
+        SSO = 0,
+        CMP = 1,
+        ProcessamentoHistorico = 2,
+        Remessa = 3,
+        PortalDigital = 4,
+        Email = 5
+    }
+}
