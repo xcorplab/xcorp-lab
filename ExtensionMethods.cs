cde242ada7a71fea5162caca5f9a9dd514e3a7d8
Alex Rondão <alexrondao@hotmail.com>
2019-10-03 11:35:14 -0300

    classe para extender do tipo lambda

---
+++ ExtensionMethods.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Printlaser.Infra.Crosscutting
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// Remove os itens nulos da coleção
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static List<T> ToNonNullList<T>(this IEnumerable<T> obj)
+        {
+            return obj == null ? new List<T>() : obj.ToList();
+        }
+
+        /// <summary>
+        /// Converte uma list de objetos (classe) para um DataTable
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="tableName"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(this IList<T> data, string tableName)
+        {
+            PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable(tableName);
+
+            foreach (PropertyDescriptor prop in properties)
+                table.Columns.Add(prop.Name, Nullable.GetUnderlyingType(prop.PropertyType) ?? prop.PropertyType);
+
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (PropertyDescriptor prop in properties)
+                    row[prop.Name] = prop.GetValue(item) ?? DBNull.Value;
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <param name="tableName"></param>
+        /// <param name="cols"></param>
+        /// <returns></returns>
+        public static DataTable ToDataTable<T>(this IList<T> data, string tableName, Dictionary<string, Type> cols)
+        {
+            //PropertyDescriptorCollection properties = TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable(tableName);
+
+            foreach (var prop in cols)
+                table.Columns.Add(prop.Key, prop.Value);
+
+            foreach (T item in data)
+            {
+                DataRow row = table.NewRow();
+                foreach (var prop in cols)
+                    row[prop.Key] = item;
+                table.Rows.Add(row);
+            }
+
+            return table;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dateToCheck"></param>
+        /// <param name="startDate"></param>
+        /// <param name="endDate"></param>
+        /// <returns></returns>
+        public static bool InRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate)
+        {
+            return dateToCheck >= startDate && dateToCheck <= endDate;
+        }
+
+        /// <summary>
+        /// Retorna a primeira data do mês com o primeiro horário do dia. Ex.: 01/01/2001 00:00:00.000
+        /// </summary>
+        /// <param name="dateToCheck"></param>
+        /// <returns></returns>
+        public static DateTime FirstDayOfMonth(this DateTime dateToCheck)
+        {
+            return new DateTime(dateToCheck.Year, dateToCheck.Month, 1);
+        }
+
+        /// <summary>
+        /// Retorna a última data do mês com o último horário do dia. Ex.: 31/01/2001 23:59:59.999
+        /// </summary>
+        /// <param name="dateToCheck"></param>
+        /// <returns></returns>
+        public static DateTime LastDayOfMonth(this DateTime dateToCheck)
+        {
+            return new DateTime(dateToCheck.Year, dateToCheck.Month, DateTime.DaysInMonth(dateToCheck.Year, dateToCheck.Month), 23, 59, 59, 997);
+        }
+
+        /// <summary>
+        /// Retorna uma lista com a primeira e última data do mês com o primeiro e último horário do dia correspondente. Ex.: 01/01/2001 00:00:00.000 e 31/01/2001 23:59:59.999
+        /// </summary>
+        /// <param name="dateToCheck"></param>
+        /// <returns></returns>
+        public static List<DateTime> RangeOfMonth(this DateTime dateToCheck)
+        {
+            return new List<DateTime>() { dateToCheck.FirstDayOfMonth(), dateToCheck.LastDayOfMonth() };
+        }
+
+        public static bool ValidarEmail(this string email)
+        {
+            bool emailValido = false;
+            string emailRegex = string.Format("{0}{1}",
+                @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))",
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$");
+
+            try
+            {
+                emailValido = Regex.IsMatch(
+                    email,
+                    emailRegex);
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                emailValido = false;
+            }
+
+            return emailValido;
+        }
+        public static bool ValidarCNPJ(this string cnpj)
+        {
+            int[] multiplicador1 = new int[12] { 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[13] { 6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int soma;
+            int resto;
+            string digito;
+            string tempCnpj;
+            cnpj = cnpj.Trim();
+            cnpj = cnpj.Replace(".", "").Replace("-", "").Replace("/", "");
+            if (cnpj.Length != 14)
+                return false;
+            tempCnpj = cnpj.Substring(0, 12);
+            soma = 0;
+            for (int i = 0; i < 12; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador1[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCnpj = tempCnpj + digito;
+            soma = 0;
+            for (int i = 0; i < 13; i++)
+                soma += int.Parse(tempCnpj[i].ToString()) * multiplicador2[i];
+            resto = (soma % 11);
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cnpj.EndsWith(digito);
+        }
+        public static bool ValidarCPF(this string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if (cpf.Length != 11)
+                return false;
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+
+            for (int i = 0; i < 9; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = resto.ToString();
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+            if (resto < 2)
+                resto = 0;
+            else
+                resto = 11 - resto;
+            digito = digito + resto.ToString();
+            return cpf.EndsWith(digito);
+        }
+
+        public static T Clone<T>(this T source)
+        {
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("The type must be serializable.", "source");
+            }
+
+            // Don't serialize a null object, simply return the default for that object
+            if (Object.ReferenceEquals(source, null))
+            {
+                return default(T);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using (stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
